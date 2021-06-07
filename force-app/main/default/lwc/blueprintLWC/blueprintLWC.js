4e04ac2fc8e74f652c4c5f46d6c0d096e3e1601a
Gibran Soto <gibransoto@gibrans-mbp.attlocal.net>
2021-06-07 08:25:50 -0500

    'push to production1'

--- force-app/main/default/lwc/blueprintLWC/blueprintLWC.js
+++ force-app/main/default/lwc/blueprintLWC/blueprintLWC.js
@@ -21,18 +21,20 @@ export default class BlueprintLWC extends LightningElement {
     Blueprint_LOGO = Blueprint_LOGO;
     UserId_var = UserId_var;
     @api isLoaded = false;
-    @track AcademicYearsList = []
-    @track AcademicYearsToDisplay = []
-    @track AcademicYearsOptionMap = {}
-    @track contactBannerID = null;
-    @track contactDOB = new Date(); 
+    @track AcademicYearsList = [];
+    @track AcademicYearsToDisplay = [];
+    @track AcademicYearsOptionMap = {};
+    // @track contactBannerID = null;
+    // @track contactDOB = new Date(); 
     @track contactVerified = false;
-    @track contactNotVerified = false;
+    @track contactNotVerified = false;     
     @track detailBlockVisible = false;
     @track AdvisorCommentsPresent = false;
+    @track isMobile = false;
 
     @track TotalHoursArray = [];
     @track TotalHoursMap = {};
+    // Constant set to 84 hours for wellness - sleeping/eating/exercising etc. 
     wellnessHours = 84;
 
     @track ReactiveGauage1 = null;
@@ -92,15 +94,18 @@ export default class BlueprintLWC extends LightningElement {
           return this.AcademicYearsOptionMap;
     }
 
+    // showSpinner() is called upon a loading event to show an animation and block the user from making any updates.
     showSpinner()
     {
         this.isLoaded = false;
     }
+
+    // updageGaugeData() is called after a BP save event - runs to recalculate gauges and rerender
     updageGaugeData(event)
     {
         var delayInMilliseconds = 1000; //1.5 second
-        var Scope_BannerID = this.contactBannerID;
-        var Scope_DOB = this.contactDOB;
+        // var Scope_BannerID = this.contactBannerID;
+        // var Scope_DOB = this.contactDOB;
         var Scope = this;
 
         setTimeout(function() {
@@ -132,14 +137,14 @@ export default class BlueprintLWC extends LightningElement {
         }, delayInMilliseconds);
 
     }
-    setBannerID(event)
-    {
-        this.contactBannerID = event.target.value;
-    }
-    setDOB(event)
-    {
-        this.contactDOB = event.target.value;
-    }
+    // setBannerID(event)
+    // {
+    //     this.contactBannerID = event.target.value;
+    // }
+    // setDOB(event)
+    // {
+    //     this.contactDOB = event.target.value;
+    // }
     
 
     contactLookupHandler()
@@ -147,38 +152,39 @@ export default class BlueprintLWC extends LightningElement {
         var Scope = this;
         var delayInMilliseconds = 7000;
         Scope.isLoaded = false;
+        //Set a timeout for loading the page 
         setTimeout(function() { 
             Scope.isLoaded = true;
             Scope.AcademicYearsToDisplay = [];
         }, delayInMilliseconds);
-        console.log('runs by contact lookup');
 
+        // Call apex class to get BP records using the userID from @salesforce/user/Id
         getBlueprintData({UserId:UserId_var}).then(response=>{
 
             var WellnessHours = this.wellnessHours;
             this.responseJSON = JSON.parse(response);
 
+            // Updaing the responseJSON response to account for WellnessHours
             this.responseJSON.BluePrintList.forEach(function (item, index) {
                 item.TotalHours_Fall = item.IIT_Total_Committed_Hours_fall__c + WellnessHours;
                 item.TotalHours_Spring = item.IIT_Total_Committed_Hours_spring__c + WellnessHours;
                 item.TotalHours_SumWin = item.IIT_Total_Committed_Hours_sumwin__c + WellnessHours;
               });            
-
+            
+            // On successful json response
             if(this.responseJSON.SuccessfulLookup == true)
             {
-                console.log('Found a valid account');
-                console.log(this.responseJSON);
-                // code to place each IIT_Academic_Year_formulaText__c in list to open accordion view -- review, does not work
+                // Placeing each IIT_Academic_Year_formulaText__c in list to open accordion view - The accordion needs to have these open breifly so the gauge.js renders.
                 this.responseJSON.BluePrintList.forEach(function (item, index) {
                     Scope.AcademicYearsList.push(item.IIT_Academic_Year_formulaText__c);
                   });
                 Scope.AcademicYearsToDisplay = Scope.AcademicYearsList;
                 this.contactVerified = true;                
             }
+            // On failed json response
             else{
                     this.contactVerified = false;
                     this.contactNotVerified = true;
-                    console.log('Did not find a valid account');
                     this.isLoaded = true;
                     const event = new ShowToastEvent({
                         title: 'Access Not Granted',
@@ -186,10 +192,6 @@ export default class BlueprintLWC extends LightningElement {
                         variant: 'error',
                     });
                     this.dispatchEvent(event);
-                    
-                
-                console.log('Did not grant entry');
-                
             }
         }).catch(error =>{
             console.error(error);
@@ -206,9 +208,9 @@ export default class BlueprintLWC extends LightningElement {
         this.detailBlockVisible = false;
     }
     
-
+    // This is called once the lwc loads
     renderedCallback() {
-        // this code runs each time markup is rendered & contact is verified
+
         if (this.contactVerified == true)
         {
             Promise.all([
@@ -280,9 +282,7 @@ export default class BlueprintLWC extends LightningElement {
                   this.gauge14 = new window.Gauge(target14).setOptions(opts);
                   this.gauge15 = new window.Gauge(target15).setOptions(opts); 
                 
-                  
                 var maxGuageValue = 168;
-
                 //   Guage1
                 this.gauge1.maxValue = maxGuageValue; // set max gauge value
                 this.gauge1.setMinValue(0);  // Prefer setter over gauge.minValue = 0
@@ -367,22 +367,31 @@ export default class BlueprintLWC extends LightningElement {
                 this.gauge15.setMinValue(0);  // Prefer setter over gauge.minValue = 0
                 this.gauge15.animationSpeed = 32; // set animation speed (32 is default value)
                 this.gauge15.set(this.responseJSON.BluePrintList[4].TotalHours_SumWin); // set actual value                  
-
               });              
 
         }
+        // Runs if the contact is not verified
         else if (this.contactVerified == false)
         {
-            this.contactLookupHandler();
+            // Checking to see if this is rendered via mobile browswer.
+            let check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            if (check == true)
+            {
+                this.isMobile = true;
+            }
+            // Call contactLookupHandler() if the app is loaded using a desktop/laptop browswer
+            else{
+                this.contactLookupHandler();
+            }            
         }
         
     }
     
     handleLogout() {
         // /secur/logout.jsp?retUrl=YourLoginpageURL
-        window.location.href = "/studentblueprint/secur/logout.jsp?retUrl=https://test-myiit.cs34.force.com/studentblueprint/";
+        window.location.href = "/studentblueprint/secur/logout.jsp?retUrl=https://test-myiit.cs34.force.com/studentblueprint/login";
 
-    }    
-
+    }
     
 }
